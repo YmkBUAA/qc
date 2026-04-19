b1cfc90c08a768e8177326e906d3ed011edbfd9c
YmkBUAA <21375280@buaa.edu.cn>
2026-04-19 23:52:36 +0800

    nacfql

--- agents/__init__.py
+++ agents/__init__.py
@@ -1,7 +1,9 @@
 from agents.acfql import ACFQLAgent
 from agents.acrlpd import ACRLPDAgent
+from agents.nacfql import NACFQLAgent
 
 agents = dict(
     acfql=ACFQLAgent,
     acrlpd=ACRLPDAgent,
+    nacfql=NACFQLAgent,
 )
